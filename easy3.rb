f596be46501fac423e98600e3ee9120c3d32ad68
Pablo Vent <ventpdev@gmail.com>
2019-10-17 22:34:14 +0100

    problem 4 completed.

---
+++ easy3.rb
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+
+numbers.delete(1)
+# p numbers
+numbers.delete(1)
+
+# delete_at, deletes the element at the specified position.
+# delete, deletes the specified value, e.g, it deletes 1.
