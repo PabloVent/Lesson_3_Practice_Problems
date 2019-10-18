0e2e4337b67d2f18cff3b458bb7eb0cf533fb107
Pablo Vent <ventpdev@gmail.com>
2019-10-18 20:10:19 +0100

    problem 8 completed.

---
+++ easy8.rb
@@ -0,0 +1,12 @@
+hsh = {"Fred"=>0, "Wilma"=>1, "Barney"=>2, "Betty"=>3, "BamBam"=>4, "Pebbles"=>5}
+hsh = hsh.to_a
+
+p hsh.flatten!
+
+hsh.select! do |idx|
+  idx == "Barney" || idx == 2
+end
+
+p hsh
+
+# p hsh.assoc("Barney")
