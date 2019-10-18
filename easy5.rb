3bd62412e78b5de0b933db94b40672734d9eab63
Pablo Vent <ventpdev@gmail.com>
2019-10-18 07:38:50 +0100

    problem 6 completed.

---
+++ easy5.rb
@@ -0,0 +1,7 @@
+# show two different ways to put the expected "Four score and " in front of it.
+
+complete_words = "Four score and"
+famous_words = "seven years ago..."
+
+p "#{complete_words} #{famous_words}"
+p complete_words + ' ' + famous_words
