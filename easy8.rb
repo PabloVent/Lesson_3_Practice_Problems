215d5ecfc10ed6cf02eebab3b81ad8aeebd825ef
Pablo Vent <ventpdev@gmail.com>
2019-10-18 20:12:24 +0100

    problem 8 update.

--- easy8.rb
+++ easy8.rb
@@ -1,7 +1,7 @@
 hsh = {"Fred"=>0, "Wilma"=>1, "Barney"=>2, "Betty"=>3, "BamBam"=>4, "Pebbles"=>5}
 hsh = hsh.to_a
 
-p hsh.flatten!
+hsh.flatten!
 
 hsh.select! do |idx|
   idx == "Barney" || idx == 2
